e95c2e28097fc597e3debee4a02888f70c02415c
Artem Kyselov <calmos.owl@gmail.com>
2025-01-17 09:38:11 +0200

    Initial Commit

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Artem Kyselov on 17.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
